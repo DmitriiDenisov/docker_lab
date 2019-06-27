757bc44a500ccddea0372758b20e4421c20d21ca
CloudDmitry <dmitryhse@gmail.com>
2019-06-27 22:25:34 +0000

    add example with docker-compose

--- example_docker_compose_flask/ignore_this_folder/main.py
+++ example_docker_compose_flask/ignore_this_folder/main.py
@@ -6,4 +6,4 @@ def hello_world():
     return 'Hello world!'
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0', port=5000) # будет доступно по http://localhost:5000
+    app.run(debug=True,host='0.0.0.0', port=80) # будет доступно по http://localhost:80
